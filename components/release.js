c275003771b9503e62bdd402fe8599e5b29face7
gcerame <g.cerame@gmail.com>
2022-10-04 22:33:28 +0200

    made cover image bigger, added artist name and reordered stuff

--- components/release.js
+++ components/release.js
@@ -9,12 +9,15 @@ export default function Release ({ release }) {
     return (
         <Container className="release" bg='gray.400' color='white' padding={4} border="1px" borderRadius="10px">
             <Heading>{release.title}</Heading>
-            <p>{release.year}</p>
-            {release.image && <Image src={release.image} width={50} height={50} objectFit="none" alt="release cover"/>}
-            {release.videos && <ReleaseVideos videos={release.videos} releaseTitle={release.title}/>}
+            <Heading size="md">{release.artists[0].name}</Heading>
             <p>Want: {release.want} Have: {release.have}</p>
             <p>For sale: {release.forSale}</p>
             <Link href={release.discogsURL}>Go to discogs</Link>
+            <p>{release.year}</p>
+            {release.image &&
+                <Image src={release.image} width={200} height={200} objectFit="none" alt="release cover"/>}
+            {release.videos && <ReleaseVideos videos={release.videos} releaseTitle={release.title}/>}
+
         </Container>
     );
 }
