ec1de857aa66bf8ea4a4f320d22b0b712f5b0628
gcerame <g.cerame@gmail.com>
2023-05-01 23:51:59 +0200

    testing framer motion

--- components/release.js
+++ components/release.js
@@ -1,12 +1,23 @@
 import 'react-lite-youtube-embed/dist/LiteYouTubeEmbed.css';
 import ReleaseVideos from "./releaseVideos";
 import ReleaseInfo from "./releaseInfo";
+import { motion } from "framer-motion";
 
 export default function Release ({ release }) {
 
     return (
-        <div className="bg-stone-400 text-gray-300 drop-shadow-2xl flex   gap-5
-         p-2 justify-between">
+        <motion.div
+            className="bg-stone-400 text-gray-300 drop-shadow-2xl flex gap-5 p-2 justify-between"
+            animate={{
+                //Fade in animation
+                opacity: [0, 1],
+                //Animate from left to middle of page using framer motion
+                x: [-1000, 0],
+
+            }}
+            transition={{ delay: 0.5 }}
+
+        >
             <ReleaseInfo release={release}/>
             {release.videos ?
 
@@ -15,7 +26,7 @@ export default function Release ({ release }) {
                 <p className="self-center">There are no videos for this release.</p>
             }
 
-        </div>
+        </motion.div>
     );
 }
 
