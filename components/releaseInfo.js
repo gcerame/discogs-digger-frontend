4de397747edb2655c306f39b13d9fd0d7b60320c
gcerame <g.cerame@gmail.com>
2023-05-28 20:58:08 +0200

    small styling fix and format

--- components/releaseInfo.js
+++ components/releaseInfo.js
@@ -6,7 +6,8 @@ import React from "react";
 
 export default function ReleaseInfo ({ release }) {
     return (
-        <div className="flex flex-col  place-content-around bg-stone-500 p-2 rounded w-full md:w-1/3 self-center noise">
+        <div className="flex flex-col place-content-around bg-stone-500 p-2 rounded w-full self-center noise
+        md:w-1/3 md:h-full">
             <div className="flex flex-col place-content-around bg-stone-600 text-center p-2
                 rounded w-full self-center">
                 {release.image &&
@@ -17,26 +18,19 @@ export default function ReleaseInfo ({ release }) {
                 <span className="italic text-xl">{release.title}</span>
                 <p>{release.year}</p>
                 <span className="text-lg">{release.artists[0].name}</span>
-            
             </div>
             <ReleaseTracklist release={release}/>
 
             <div className="flex flex-col text-center p-2">
-
-
                 {
                     release.want ? <p>Want: {release.want} Have: {release.have}</p> : null
                 }
-
                 <p>For sale: {release.forSale}</p>
                 <p>Lowest price on marketplace {release.lowestPrice} €</p>
                 <Button className=" bg-stone-800 active:translate-y-1 w-fit p-1 m-1.5 self-center"
                     text={<Link href={release.discogsURL}>discogs↗</Link>}>
                 </Button>
-
-
             </div>
-
         </div>
     );
 }
