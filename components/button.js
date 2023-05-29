e7a338301349d6e7aead38e62bb570126a7076b2
gcerame <g.cerame@gmail.com>
2023-05-29 20:31:56 +0200

    button style change

--- components/button.js
+++ components/button.js
@@ -1,8 +1,8 @@
 export default function Button ({ text, onClick,type, disabled }) {
     return (
         <button
-            className="bg-neutral-700 p-3 m-2 border-1 border-black drop-shadow-lg focus:ring hover:ring
-                         transition-all active:transform active:translate-y-1 w-fit self-center "
+            className="bg-neutral-700 p-3 m-1 border-1 border-black drop-shadow-lg focus:ring hover:ring
+                         transition-all active:transform active:translate-y-1  self-center"
             type={type ? type : null}
             onClick={onClick}
             aria-label={text}
