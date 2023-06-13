31175775ead01a197617291ca1a1c5d5566b1467
gcerame <g.cerame@gmail.com>
2023-06-13 13:33:31 +0200

    fixed pageNavigation.js to use Button component

--- components/pageNavigation.js
+++ components/pageNavigation.js
@@ -1,8 +1,7 @@
 import Button from "./button";
 
 export default function PageNavigation ({ handlePreviousPage, handleNextPage, pagination }) {
-    const buttonClassName = "bg-neutral-700 p-3 m-2 border-1 border-black drop-shadow-lg focus:ring hover:ring" +
-        " transition-all active:transform active:translate-y-1 w-fit self-center";
+
 
     return (
         <div className="navigation-menu flex flex-col self-center">
@@ -10,9 +9,8 @@ export default function PageNavigation ({ handlePreviousPage, handleNextPage, pa
 
             <div className="">
                 <Button disabled={pagination.page === 1} onClick={handlePreviousPage} text="Previous"/>
-                <button disabled={pagination.page === pagination.pages} onClick={handleNextPage}
-                    className={buttonClassName}>Next
-                </button>
+                <Button disabled={pagination.page === pagination.pages} onClick={handleNextPage} text="Next">
+                </Button>
             </div>
         </div>
     );
