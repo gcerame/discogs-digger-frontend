4e38086307c9ea7ae80a5822f1c20e9e56feb8b8
ceramesupersound <g.cerame@gmail.com>
2023-06-22 09:01:34 +0200

    changed app name on header

--- components/header.js
+++ components/header.js
@@ -8,7 +8,7 @@ export function Header () {
         }}>
             <div className="flex align-center justify-center px-4 py-6 font-normal flex-wrap">
                 <h1 className="text-4xl text-shadow md:text-5xl ">
-                    <span className="text-neutral-800">discogs</span>digger
+                    <span className="text-neutral-800">guillermo</span>digger
                 </h1>
             </div>
         </motion.div>
